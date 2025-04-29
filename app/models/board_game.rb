1c3bc5d5f943bc39dfc5f0e5593504d18135f726
Karen Luna <kluna1@memphis.edu>
2025-04-28 19:46:32 -0500

    Added Validations To BoardGame Model

--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -1,2 +1,5 @@
 class BoardGame < ApplicationRecord
-end
+    validates :title, presence: true, uniqueness: true
+    validates :category, :publisher, :description, presence: true
+    validates :min_players, :max_players, numericality: { greater_than: 0 }
+end  
